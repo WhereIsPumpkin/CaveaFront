a8844d2da2d6211255a587bb05c3514cbec371d8
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-05-05 13:37:48 +0400

    add react router + delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,13 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-
-interface Item {
-  name: string;
-  location: string;
-  price: number;
-}
+import HomePage from './HomePage';
+import AddItem from './AddItem';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  const [items, setItems] = useState<Item[]>([
-    {
-      name: 'Item 1',
-      location: 'Location 1',
-      price: 100
-    }
-  ]);
-
-  const handleDeleteItem = (index: number) => {
-    setItems(items.filter((_, i) => i !== index));
-  };
-
   return (
-    <div className="container mt-5">
-      <h1 className="text-center mb-4">Inventory Management</h1>
-      <div className="d-flex justify-content-end mb-4">
-        <Button variant="primary" href="/add">Add Item</Button>
-      </div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Item Name</th>
-            <th>Item Location</th>
-            <th>Price (in GEL)</th>
-            <th>Operations</th>
-          </tr>
-        </thead>
-        <tbody>
-          {items.map((item, index) => (
-            <tr key={index}>
-              <td>{item.name}</td>
-              <td>{item.location}</td>
-              <td>{item.price}</td>
-              <td><Button variant="danger" onClick={() => handleDeleteItem(index)}>Delete</Button></td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add" element={<AddItem />} />
+      </Routes>
   );
 }
 
---
+++ src/HomePage.tsx
@@ -0,0 +1,109 @@
+import { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+
+interface Item {
+  id: number;
+  name: string;
+  location: string;
+  price: number;
+}
+
+function App() {
+  const [items, setItems] = useState<Item[]>([]);
+  const [locationFilter, setLocationFilter] = useState('All');
+  const [sortBy, setSortBy] = useState('name');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      let url = 'http://127.0.0.1:4444/inventories';
+
+      if (locationFilter !== 'All') {
+        url += `?location=${locationFilter}`;
+      }
+
+      if (sortBy) {
+        url += `${url.includes('?') ? '&' : '?'}sortBy=${sortBy}`;
+      }
+
+      const response = await axios.get(url);
+      setItems(response.data);
+    };
+
+    fetchItems();
+  }, [locationFilter, sortBy]);
+
+  function handleDelete(id: number) {
+    axios.delete(`http://127.0.0.1:4444/inventories/${id}`)
+      .then(() => {
+        setItems(items.filter(item => item.id !== id));
+      });
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Inventory Management</h1>
+      <div className="d-flex justify-content-end mb-4">
+        <Button variant="primary" href="/add">ნივთის დამატება</Button>
+      </div>
+      <div className="d-flex mb-4">
+        <div className="me-4">
+          <label htmlFor="locationFilter" className="form-label">ადგილმდებარეობა</label>
+          <select
+            id="locationFilter"
+            className="form-select"
+            value={locationFilter}
+            onChange={e => setLocationFilter(e.target.value)}
+          >
+            <option value="all">ყველა</option>
+            <option value="mainOffice">მთავარი ოფისი</option>
+            <option value="caveaGallery">კავეა გალერია</option>
+            <option value="caveaTbilisiMall">კავეა თბილისი მოლი</option>
+            <option value="caveaEastPoint">კავეა ისთ ფოინთი</option>
+            <option value="caveaCityMall">კავეა სითი მოლი</option>
+            
+          </select>
+        </div>
+        <div className="d-flex align-items-end">
+          <Button
+            variant={sortBy === 'name' ? 'primary' : 'secondary'}
+            onClick={() => setSortBy('name')}
+          >
+            Sort by name
+          </Button>
+          <Button
+            variant={sortBy === 'price' ? 'primary' : 'secondary'}
+            onClick={() => setSortBy('price')}
+            className="ms-2"
+          >
+            Sort by price
+          </Button>
+        </div>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ნივთის სახელი</th>
+            <th>ნივთის ადგილმდებარეობა</th>
+            <th>ფასი (ლარებში)</th>
+            <th>ოპერაციები</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <tr key={index}>
+              <td>{item.name}</td>
+              <td>{item.location}</td>
+              <td>{item.price}</td>
+              <td><Button variant="danger" onClick={() => handleDelete(item.id)}>წაშლა</Button></td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+}
+
+export default App;
